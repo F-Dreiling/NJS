6d4526dfb8cb457cb40cbe2d2d104f1fbd11fb13
FX Drooklyn <dr.f380@hotmail.com>
2024-04-12 18:37:37 +0200

    setup

--- app.js
+++ app.js
@@ -10,4 +10,10 @@ const items = [1, [2, [3, [4]]]];
 const newItems = _.flattenDeep(items);
 
 console.log(newItems);
-console.log('hello world')
+console.log('hello world');
+
+setTimeout(() => {
+    console.log('callback');
+}, 0);
+
+console.log('finished');
