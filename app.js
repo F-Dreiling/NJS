33ccfa21437dcd1ee4a459fbf8f27f14e9c22845
FX Drooklyn <dr.f380@hotmail.com>
2024-08-22 17:20:21 +0200

    Streams

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const http = require('http')
+var http = require('http')
+var fs = require('fs')
 
-// const server = http.createServer((req, res) => {
-//   res.end('Welcome')
-// })
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
 
-// Using Event Emitter API
-const server = http.createServer()
-// emits request event
-// subcribe to it / listen for it / respond to it
-server.on('request', (req, res) => {
-  res.end('Welcome')
-})
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
 
-server.listen(5000)
+    fileStream.on('open', () => {
+      fileStream.pipe(res)
+    })
+
+    fileStream.on('error', (err) => {
+      res.end(err)
+    })
+  })
+  .listen(5000)
