f517e1a02c1a6020b15640c40a06c1ee02a8bc1a
FX Drooklyn <dr.f380@hotmail.com>
2024-10-26 11:47:59 +0200

    Params / Query

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-
 const { products } = require('./data')
 
 app.get('/', (req, res) => {
@@ -13,6 +12,43 @@ app.get('/api/products', (req, res) => {
     return {id, name, image}
   })
   res.json(newProducts)
+})
+
+app.get('/api/products/:productID', (req, res) => {
+  const { productID } = req.params
+  
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  )
+
+  if (!singleProduct) {
+    return res.status(404).send('Product does not exist')
+  }
+
+  res.json(singleProduct)
+})
+
+app.get('/api/v1/query', (req, res) => {
+  console.log(req.query)
+
+  const { search, limit } = req.query
+  let sortedProducts = [...products]
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search)
+    })
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+
+  if (sortedProducts.length < 1) {
+    return res.status(200).json({success: true, data: []})
+  }
+
+  res.status(200).json(sortedProducts)
 })
 
 app.all('*', (req, res) => {
