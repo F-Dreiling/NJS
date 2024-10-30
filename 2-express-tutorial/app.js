8d83f90322b458f11531013ad576c44ef9fb769b
FX Drooklyn <dr.f380@hotmail.com>
2024-10-30 15:01:53 +0100

    Router

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 
-//Routes
-app.get('/', (req, res) => {
-  res.send('Home Page')
-})
+const peopleRouter = require('./routes/people')
+const authRouter = require('./routes/auth')
 
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use('/api/people', peopleRouter)
+app.use('/login', authRouter)
+
+//Routes
 app.all('*', (req, res) => {
   res.status(404).send('resource not found')
 })
